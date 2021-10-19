4218a6b5a5820f55e172da6bf4f997f421d9dadf
Clark19 <clarkgable1@gmail.com>
2021-10-20 01:36:57 +0900

    Refactoring : Update 컴포넌트의 input 박스 입력시 불필요한 렌더링 없앰 - onChange방식 => OnClick 방식으로 state 안쓰는 방식으로 변경

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,33 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import { useParams } from 'react-router';
 
 export const Update = ({onUpdate, topics}) => {
+  const titleRef = useRef();
+  const bodyRef = useRef();
   const params = useParams()
   const id = parseInt(params.id)
   const topic = topics.find(item => item.id === id);
-  const [content, setContent] = useState(topic)
 
   useEffect(() => console.log('Update Comp 랜더링 호출 확인용'));
 
   const onUpdateClick = (e) => {
     e.preventDefault();
-    onUpdate(content);
+    topic[titleRef.current.name] = titleRef.current.value
+    topic[bodyRef.current.name] = bodyRef.current.value
+    onUpdate(topic);
   };
-
-  function onChange(e) {
-    const {name, value} = e.target;
-    setContent(cur => {
-      const newContent = {...cur}
-      newContent[name] = value
-      return newContent
-    });
-
-  }
 
   return (
   <div className='App'>(CreateComp)
-    <div><input name='title' value={content.title} onChange={onChange} /></div>
-    <div><input name='body' value={content.body} onChange={onChange} /></div>
+    <div><input name='title' ref={titleRef} defaultValue={topic.title} /></div>
+    <div><input name='body' ref={bodyRef} defaultValue={topic.body} /></div>
     <div><button onClick={onUpdateClick}>Update</button></div>
   </div>);
 };
